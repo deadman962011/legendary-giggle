8644c5457f20acea0a68b81b27c1f866ebbbbdf1
Ahmad al-aswad <ahmad.alaswad.dev@gmail.com>
2024-05-06 19:22:02 +0300

    5/6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'api.localization' => \App\Http\Middleware\ApiLocalizationMiddleWare::class,
-        'validated_user_location' => \App\Http\Middleware\Api\validateUserLocation::class,
+        'validated_user_location' => \App\Http\Middleware\Api\ValidateUserLocation::class,
     ];
 }
